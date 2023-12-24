dd8b973da158288f57ef5de9672216df689e9a1b
ahmetkocar <ahmetkocar@gmail.com>
2023-12-24 23:09:27 +0300

    Implement card flip animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,17 +11,47 @@ export default function Card({ card, streak, setStreak }) {
 
   return (
     <motion.div
-      className="pb-3 max-w-[15rem] bg-stone-800 text-blue-200 ring-2 ring-stone-500 shadow-xl shadow-slate-500 rounded-lg space-y-2 cursor-pointer"
+      className="relative max-w-[15rem] w-full aspect-[3/4] bg-stone-800 text-blue-200 ring-2 ring-stone-500 shadow-xl shadow-slate-500 rounded-lg cursor-pointer [transform-style:preserve-3d]"
       whileHover={{
         scale: 1.05,
         transition: { duration: 0.15 },
       }}
-      initial={{ opacity: 0, y: -50, transition: { duration: 0.7 } }}
-      animate={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
+      initial={{
+        rotateY: 180,
+      }}
+      animate={{
+        rotateY: 0,
+        transition: {
+          type: "spring",
+          damping: 15,
+          stiffness: 110,
+        },
+      }}
+      exit={{
+        rotateY: 180,
+        transition: {
+          type: "spring",
+          damping: 15,
+          stiffness: 110,
+        },
+      }}
       onClick={handleClick}
     >
-      <img src={card.src} className="rounded-lg" draggable="false"></img>
-      <p className="text-center font-medium">{card.title}</p>
+      <div className="absolute w-full h-full space-y-2 pb-4 [backface-visibility:hidden]">
+        <img
+          src={card.src}
+          className="rounded-lg w-full h-[90%]"
+          draggable="false"
+        ></img>
+        <p className="text-center font-medium">{card.title}</p>
+      </div>
+      <div className="absolute w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden]">
+        <img
+          src="/assets/self-portrait.jpg"
+          className="rounded-lg h-full"
+          draggable="false"
+        ></img>
+      </div>
     </motion.div>
   );
 }
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import cards from "../constants/cards.js";
 import cardShuffler from "../constants/cardShuffler.js";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function CardGrid({ streak, setStreak }) {
   const shuffledCards = cardShuffler(cards);
@@ -20,15 +21,22 @@ export default function CardGrid({ streak, setStreak }) {
   }
 
   return (
-    <div className="p-16 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 justify-items-center">
-      {shuffledCards.map((card) => (
-        <Card
-          key={Math.random()}
-          card={card}
-          streak={streak}
-          setStreak={setStreak}
-        />
-      ))}
-    </div>
+    <motion.div
+      className="p-16 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 justify-items-center"
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <AnimatePresence mode="wait" initial={false}>
+        {shuffledCards.map((card) => (
+          <Card
+            key={Math.random()}
+            card={card}
+            streak={streak}
+            setStreak={setStreak}
+          />
+        ))}
+      </AnimatePresence>
+    </motion.div>
   );
 }
